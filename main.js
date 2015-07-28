42e527cd579ab32622d9a16a1507bc0de0fbdb94
Peter Connolly <pmconnolly80@gmail.com>
2015-07-28 10:53:30 -0500

    turn in the quiz

---
+++ main.js
@@ -0,0 +1,25 @@
+var firstA = [];
+firstA.sort();
+
+var secondA = [];
+secondA.sort(function(){
+  
+  for (var i = 0; i < secondA.length; i++) {
+  return secondA[i] > 25;
+};
+
+var button = $('input').click(function(){
+  return secondA();
+  console.log(button);
+});
+
+var strInput = [];
+var newStrInput = strInput.split(',');
+  for (var j=0; i<newStrInput.length; j++);
+    newStrInput[j]=parseInt(newStrInput[i]);
+  console.log(newStrInput);
+});
+
+console.log(secondA);
+
+
